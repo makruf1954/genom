2adf3e98cb3afd872ccdb03ac190815c68b0fdf4
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-06-15 22:20:19 +0700

    Update insshws.sh

--- WEBSOCKET/insshws.sh
+++ WEBSOCKET/insshws.sh
@@ -3,7 +3,7 @@
 
 
 
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-ws/ws-dropbear
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/WEBSOCKET/ws-dropbear
 chmod +x /usr/local/bin/ws-dropbear
 
 # Installing Service
@@ -31,7 +31,7 @@ systemctl enable ws-dropbear.service
 systemctl start ws-dropbear.service
 systemctl restart ws-dropbear.service
 
-wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-ws/ws-ovpn.py
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/WEBSOCKET/ws-ovpn.py
 chmod +x /usr/local/bin/ws-ovpn
 
 # Installing Service
@@ -58,7 +58,7 @@ systemctl daemon-reload
 systemctl enable ws-ovpn
 systemctl restart ws-ovpn
 
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-ws/ws-stunnel
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/WEBSOCKET/ws-stunnel
 chmod +x /usr/local/bin/ws-stunnel
 
 # Installing Service
