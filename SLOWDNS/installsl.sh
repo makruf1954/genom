48b90d693e799abfaf44f95fce2307274c0a1547
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-06-15 22:06:44 +0700

    Update installsl.sh

--- SLOWDNS/installsl.sh
+++ SLOWDNS/installsl.sh
@@ -67,15 +67,15 @@ ns_domain_cloudflare() {
 setup_dnstt() {
 	cd
 	mkdir -p /etc/slowdns
-	wget -O dnstt-server "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-DnsTT/dnstt-server" >/dev/null 2>&1
+	wget -O dnstt-server "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SLOWDNS/dnstt-server" >/dev/null 2>&1
 	chmod +x dnstt-server >/dev/null 2>&1
-	wget -O dnstt-client "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-DnsTT/dnstt-client" >/dev/null 2>&1
+	wget -O dnstt-client "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main//SLOWDNS/dnstt-client" >/dev/null 2>&1
 	chmod +x dnstt-client >/dev/null 2>&1
 	./dnstt-server -gen-key -privkey-file server.key -pubkey-file server.pub
 	chmod +x *
 	mv * /etc/slowdns
-	wget -O /etc/systemd/system/client.service "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-DnsTT/client" >/dev/null 2>&1
-	wget -O /etc/systemd/system/server.service "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-DnsTT/server" >/dev/null 2>&1
+	wget -O /etc/systemd/system/client.service "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SLOWDNS/client" >/dev/null 2>&1
+	wget -O /etc/systemd/system/server.service "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SLOWDNS/cserver" >/dev/null 2>&1
 	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/client.service 
 	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/server.service 
 }
