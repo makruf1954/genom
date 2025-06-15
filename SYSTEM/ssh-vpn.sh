017323f7ceefb354fde6ab9613c9d0a9385afe1c
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-06-15 22:13:27 +0700

    Update ssh-vpn.sh

--- SYSTEM/ssh-vpn.sh
+++ SYSTEM/ssh-vpn.sh
@@ -26,7 +26,7 @@ commonname=none
 email=none
 
 # simple password minimal
-curl -sS https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
+curl -sS https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTEM/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
 chmod +x /etc/pam.d/common-password
 
 # go to root
@@ -138,8 +138,8 @@ install_ssl(){
 apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-curl https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/nginx.conf > /etc/nginx/nginx.conf
-curl https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/vps.conf > /etc/nginx/conf.d/vps.conf
+curl https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTEM/nginx.conf > /etc/nginx/nginx.conf
+curl https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTEM/vps.conf > /etc/nginx/conf.d/vps.conf
 sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
 useradd -m vps;
 mkdir -p /home/vps/public_html
@@ -147,16 +147,16 @@ echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
 cd /home/vps/public_html
-wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/index.html1"
+wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTEM/index.html1"
 /etc/init.d/nginx restart
 
 # install badvpn
 cd
-wget -O /usr/sbin/badvpn "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/badvpn" >/dev/null 2>&1
+wget -O /usr/sbin/badvpn "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTEM/badvpn" >/dev/null 2>&1
 chmod +x /usr/sbin/badvpn > /dev/null 2>&1
-wget -q -O /etc/systemd/system/badvpn1.service "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/badvpn1.service" >/dev/null 2>&1
-wget -q -O /etc/systemd/system/badvpn2.service "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/badvpn2.service" >/dev/null 2>&1
-wget -q -O /etc/systemd/system/badvpn3.service "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/badvpn3.service" >/dev/null 2>&1
+wget -q -O /etc/systemd/system/badvpn1.service "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/badvpn1.service" >/dev/null 2>&1
+wget -q -O /etc/systemd/system/badvpn2.service "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/badvpn2.service" >/dev/null 2>&1
+wget -q -O /etc/systemd/system/badvpn3.service "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/badvpn3.service" >/dev/null 2>&1
 systemctl disable badvpn1 
 systemctl stop badvpn1 
 systemctl enable badvpn1
@@ -198,7 +198,7 @@ apt -y install squid3
 
 # install squid for debian 11
 apt -y install squid
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/proxy3.js"
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/proxy3.js"
 sed -i $MYIP2 /etc/squid/squid.conf
 
 # setting vnstat
@@ -257,10 +257,10 @@ sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 /etc/init.d/stunnel4 restart
 
 #OpenVPN
-wget https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
+wget https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
 
 # // install lolcat
-wget https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/lolcat.sh &&  chmod +x lolcat.sh && ./lolcat.sh
+wget https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/lolcat.sh &&  chmod +x lolcat.sh && ./lolcat.sh
 
 # memory swap 1gb
 cd
@@ -306,10 +306,10 @@ echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
 # Ganti Banner
-wget -O /etc/issue.net "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/issue.net"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/issue.net"
 
 #install bbr dan optimasi kernel
-wget https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+wget https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 
 # blokir torrent
 iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
@@ -333,10 +333,10 @@ netfilter-persistent reload
 
 # download script
 cd /usr/bin
-wget -O issue "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/issue.net"
-wget -O m-theme "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/menu/m-theme.sh"
-wget -O speedtest "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/speedtest_cli.py"
-wget -O xp "https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-System/xp.sh"
+wget -O issue "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/issue.net"
+wget -O m-theme "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/menu/m-theme.sh"
+wget -O speedtest "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/speedtest_cli.py"
+wget -O xp "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTTEM/xp.sh"
 
 chmod +x issue
 chmod +x m-theme
