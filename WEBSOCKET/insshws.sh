9b7429995e7195858ad63ec3b825f88efceb8ae6
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-06-15 14:40:38 +0700

    Add files via upload

---
+++ WEBSOCKET/insshws.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# Proxy For Edukasi & Imclass
+
+
+
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-ws/ws-dropbear
+chmod +x /usr/local/bin/ws-dropbear
+
+# Installing Service
+cat > /etc/systemd/system/ws-nontls.service << END
+[Unit]
+Description=Python Proxy Mod By SFUSION
+Documentation=https://t.me/abecasdee
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-dropbear
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
+
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-ws/ws-ovpn.py
+chmod +x /usr/local/bin/ws-ovpn
+
+# Installing Service
+cat > /etc/systemd/system/ws-ovpn.service << END
+[Unit]
+Description=Python Proxy Mod By HOKAGE LEGEND
+Documentation=https://t.me/hokagelegend1
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 2086
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-ovpn
+systemctl restart ws-ovpn
+
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/hokagelegend9999/gas/refs/heads/main/SKT-ws/ws-stunnel
+chmod +x /usr/local/bin/ws-stunnel
+
+# Installing Service
+cat > /etc/systemd/system/ws-stunnel.service << END
+[Unit]
+Description=Python Proxy Mod By HOKAGE LEGEND
+Documentation=https://t.me/hokagelegend1
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-stunnel
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
