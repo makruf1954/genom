2a3dd8402b4e33738bcb76d921e521fba545a533
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-06-16 17:29:03 +0700

    Update installsl.sh

--- SLOWDNS/installsl.sh
+++ SLOWDNS/installsl.sh
@@ -19,13 +19,13 @@ sleep 3
 cd
 
 ns_domain_cloudflare() {
-	DOMAIN=profesorhokage.my.id
+	DOMAIN=hokagelegend.web.id
 	DOMAIN_PATH=$(cat /etc/xray/domain)
 	SUB=$(tr </dev/urandom -dc a-z0-9 | head -c7)
 	SUB_DOMAIN=${SUB}.profesorhokage.my.id
 	NS_DOMAIN=ns.${SUB_DOMAIN}
-	CF_ID=dedenirwansyah90@gmail.com
-        CF_KEY=2affde1c2638841171d852fd8c60987f57c73
+	CF_ID=faridaumiabi@gmail.com
+        CF_KEY=ab8d0901acb186291e58f21359d07e7b847dc
 	set -euo pipefail
 	IP=$(wget -qO- ipinfo.io/ip)
 	echo "Updating DNS NS for ${NS_DOMAIN}..."
@@ -75,7 +75,7 @@ setup_dnstt() {
 	chmod +x *
 	mv * /etc/slowdns
 	wget -O /etc/systemd/system/client.service "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SLOWDNS/client" >/dev/null 2>&1
-	wget -O /etc/systemd/system/server.service "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SLOWDNS/cserver" >/dev/null 2>&1
+	wget -O /etc/systemd/system/server.service "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SLOWDNS/server" >/dev/null 2>&1
 	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/client.service 
 	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/server.service 
 }
