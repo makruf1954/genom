9c8a19b365ec89be5619379caf50a269a4771a54
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-06-19 01:28:42 +0700

    Update ssh-vpn.sh

--- SYSTEM/ssh-vpn.sh
+++ SYSTEM/ssh-vpn.sh
@@ -140,14 +140,14 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 curl https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTEM/nginx.conf > /etc/nginx/nginx.conf
 curl https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTEM/vps.conf > /etc/nginx/conf.d/vps.conf
-sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+sudo sed -i 's|listen = /var/run/php-fpm.sock|listen = 127.0.0.1:9000|g' /etc/php/8.3/fpm/pool.d/www.conf
 useradd -m vps;
 mkdir -p /home/vps/public_html
 echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
 cd /home/vps/public_html
-wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTEM/index.html1"
+wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/SYSTEM/index.html"
 /etc/init.d/nginx restart
 
 # install badvpn
