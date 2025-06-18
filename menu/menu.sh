996dbac6f8b8d1cf124b33ecde339034b21cf5de
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-06-19 00:44:46 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -41,7 +41,7 @@ check_ip_and_get_info() {
 # =============================================
 
 # Ambil data dari GitHub dengan timeout
-permission_file=$(curl -s --connect-timeout 10 https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/izin.txt)
+permission_file=$(curl -s --connect-timeout 10 https://raw.githubusercontent.com/hokagelegend9999/ijin/refs/heads/main/gnome)
 
 # Validasi file permission
 if [ -z "$permission_file" ]; then
