3126e3685b9dd4e18738ad1863f7206f9d594cec
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-07-14 23:28:51 +0700

    Update m-update.sh

--- menu/m-update.sh
+++ menu/m-update.sh
@@ -63,6 +63,8 @@ rm -rf insshws
 rm -rf trojan-online
 rm -rf online
 rm -rf ceklimit
+rm -rf online-xray
+rm -rf online-trojan
 
 fun_bar() {
     CMD[0]="$1"
@@ -125,7 +127,9 @@ wget -q -O /usr/bin/trial "https://raw.githubusercontent.com/hokagelegend9999/ge
 wget -q -O /usr/bin/trojan-online "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/menu/trojan-online" && chmod +x /usr/bin/trojan-online
 wget -q -O /usr/bin/ceklimit "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/menu/ceklimit" && chmod +x /usr/bin/ceklimit
 wget -q -O /usr/bin/online "https://raw.githubusercontent.com/hokagelegend9999/genom/refs/heads/main/menu/online" && chmod +x /usr/bin/online
-wget -q -O /usr/bin/atur-backup " https://github.com/hokagelegend9999/genom/raw/refs/heads/main/menu/atur-backup" && chmod +x /usr/bin/atur-backup
+wget -q -O /usr/bin/atur-backup "https://github.com/hokagelegend9999/genom/raw/refs/heads/main/menu/atur-backup" && chmod +x /usr/bin/atur-backup
+wget -q -O /usr/bin/online-xray "https://github.com/hokagelegend9999/genom/raw/refs/heads/main/menu/online-xray" && chmod +x /usr/bin/online-xray
+wget -q -O /usr/bin/online-trojan "https://github.com/hokagelegend9999/genom/raw/refs/heads/main/menu/online-trojan" && chmod +x /usr/bin/online-trojan
 
 chmod +x m-tcp
 chmod +x m-theme
@@ -174,6 +178,8 @@ chmod +x insshws
 chmod +x trojan-online
 chmod +x ceklimit
 chmod +x online
+chmod +x online-xray
+chmod +x online-trojan
 clear
 
 }
