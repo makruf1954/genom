7cfadfccc429787272214de92709f677947a1e14
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-07-17 10:49:05 +0700

    Update installsl.sh

--- SLOWDNS/installsl.sh
+++ SLOWDNS/installsl.sh
@@ -23,7 +23,7 @@ ns_domain_cloudflare() {
 	DOMAIN_PATH=$(cat /etc/xray/domain)
 	SUB=$(tr </dev/urandom -dc a-z0-9 | head -c7)
 	SUB_DOMAIN=${SUB}.hokagelegend.web.id
-	NS_DOMAIN=ns.${SUB_DOMAIN}
+	NS_DOMAIN=${SUB_DOMAIN}.sg-ujicoba.hokagelegend.web.id
 	CF_ID=faridaumiabi@gmail.com
         CF_KEY=ab8d0901acb186291e58f21359d07e7b847dc
 	set -euo pipefail
