85c3207fdcf4ad7ee63d7b6f6d246b2d3cd02b2e
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-07-22 13:50:18 +0700

    Update m-vmess.sh

--- menu/m-vmess.sh
+++ menu/m-vmess.sh
@@ -495,6 +495,14 @@ echo -e "$COLOR1 ◇━━━━━━━━━━━━━━━━━◇ ${NC}
 echo -e "$COLOR1 ${NC} ${WH}  • HOKAGE LEGEND STORE •     " | tee -a /etc/vmess/akun/log-create-${user}.log
 echo -e "$COLOR1 ◇━━━━━━━━━━━━━━━━━◇ ${NC}" | tee -a /etc/vmess/akun/log-create-${user}.log
 echo "" | tee -a /etc/vmess/akun/log-create-${user}.log
+mkdir -p /etc/vmess/akun
+cat > /etc/vmess/akun/$user << EOF
+username=$user
+limit_quota=${Quota}GB
+usage_quota=0MB
+login_time=$(date +'%H:%M:%S.%N')
+login_ip=0
+EOF
 systemctl restart xray > /dev/null 2>&1
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
