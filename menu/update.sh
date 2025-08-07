f8cc3a77a37c9885413ac3200fe15aeded754050
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-08-07 13:40:16 +0700

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -139,6 +139,7 @@ wget -q -O /usr/bin/online-vless "https://github.com/hokagelegend9999/genom/raw/
 wget -q -O /usr/bin/online-trojan "https://github.com/hokagelegend9999/genom/raw/refs/heads/main/menu/online-trojan" && chmod +x /usr/bin/online-trojan
 wget -q -O /usr/bin/online-sshws "https://github.com/hokagelegend9999/genom/raw/refs/heads/main/online-sshws" && chmod +x /usr/bin/online-sshws
 wget -q -O /usr/bin/update-usage "https://github.com/hokagelegend9999/genom/raw/refs/heads/main/menu/update-usage" && chmod +x /usr/bin/update-usage
+wget -q -O /usr/bin/update-ssh-usage "https://github.com/hokagelegend9999/genom/raw/refs/heads/main/menu/update-ssh-usage.sh" && chmod +x /usr/bin/update-ssh-usage
 
 
 clear
